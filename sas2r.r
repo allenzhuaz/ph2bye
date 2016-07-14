e1399971d5be171cd9b7c91307e93678411fbc89
Yalin (Allen) <allenzhuaz@users.noreply.github.com>
2016-07-14 15:57:30 -0400

    Update sas2r.r

--- sas2r.r
+++ sas2r.r
@@ -2,6 +2,8 @@
 library(Hmisc)
 mydata <- sasxport.get("c:/mydata.xpt")
 
-# An easier way using 'haven' package
+# An easier way using 'haven' package, the import data type is dataframe
+# so we can use 'dplyr' to manipulate the data.
 library(haven)
-mydata <- read_sas("<path to your SAS file>")
+mydata <- read_sas("c://mydata.sas7bdat")
+
