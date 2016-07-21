27119148927a81001ac91def3be8dc9be11b0b85
Yalin (Allen) <allenzhuaz@users.noreply.github.com>
2016-07-21 18:19:05 -0400

    Update postp.R

--- ph2bye/R/postp.R
+++ ph2bye/R/postp.R
@@ -1,24 +1,21 @@
-#' The posterior probability criterion function
+#' The posterior probability criterion function for Phase II single-arm design
 #'
-#' Thall and Simon's criterion function for determining the
-#' trial decision cutoffs based on the posterior probability.
+#' Thall and Simon's criterion function for determining the trial decision boundaries based on the posterior probability.
 #'
 #' @usage
-#' postprob(y, n, alpha_e, beta_e, alpha_s, beta_s, delta)
+#' PostP(x, n, a, b, p0)
 #' @param x the number of responses among \eqn{n} patients treated by the experimental drug at a certain stage of the trial.
 #' @param n the number of patients treated by the experimental drug at a certain stage of the trial.
 #' @param a the hyperparameter (shape1) of the Beta prior for the experimental drug.
 #' @param b the hyperparameter (shape2) of the Beta prior for the experimental drug.
-#' @param alpha_s the hyperparameter (shape1) of the Beta prior for the standard drug.
-#' @param beta_s the hyperparameter (shape2) of the Beta prior for the standard drug.
-#' @param delta the minimally acceptable increment of the response rate for the experimental drug compared with the standard drug.
+#' @param p0 the prespecified reseponse rate.
 #' @return
-#' \item{prob}{the posterior probability: \eqn{Pr(p_E > p_S + \delta | y)}}
+#' \item{prob}{the posterior probability: \eqn{Pr(p > p_0  | X=x)}}
 #' @references
-#' Berry, S. M., Carlin, B. P., Lee, J. J., & Muller, P. (2010). 
+#' Berry, S. M., Carlin, B. P., Lee, J. J., & Muller, P. (2010).
 #' \emph{Bayesian adaptive methods for clinical trials.}
 #' CRC press.
-#' 
+#'
 #' Thall, P. F., Simon, R. (1994).
 #' Practical Bayesian guidelines for phase IIB clinical trials.
 #' \emph{Biometrics} \strong{50}: 337-349.
@@ -26,32 +23,30 @@
 #' Yin, G. (2013).
 #' \emph{Clinical Trial Design: Bayesian and Frequentist Adaptive Methods.}
 #' New York: Wiley.
-#' @importFrom stats integrate pbeta dbeta
+#' @examples
+#' PostP(8,15,1,1,0.8)
+#' @importFrom stats pbeta
 #' @export
-PostProb <- function(x, n, a, b, p0) {
-  
+PostP <- function(x, n, a, b, p0) {
   pbeta(p0, a+x, b+n-x, lower.tail = F)
-
 }
 
 
 
 
-#' The stopping boundaries based on Thall and Simon's criterion
+#' The stopping boundaries based on the posterior probability criterion
 #'
-#' The stopping boundaries based on Thall and Simon's criterion.
+#' The design function to calucluate the sequential monitor sample size and boundary based on Thall and Simon's criterion.
 #'
 #' @usage
-#' stopbound_post(theta, type, nmax, alpha_e, beta_e, alpha_s, beta_s, delta)
-#' @param theta the cutoff probability: typically, \eqn{\theta = [0.95, 0.99]} for superiority, \eqn{\theta = [0.01, 0.05]} for futility.
+#' PostP.design(type, nmax, a, b, p0, delta, theta)
 #' @param type type of boundaries: "superiority" or "futility".
 #' @param nmax the maximum number of patients treated by the experimental drug.
-#' @param alpha_e the hyperparameter (shape1) of the Beta prior for the experimental drug.
-#' @param beta_e the hyperparameter (shape2) of the Beta prior for the experimental drug.
-#' @param alpha_s the hyperparameter (shape1) of the Beta prior for the standard drug.
-#' @param beta_s the hyperparameter (shape2) of the Beta prior for the standard drug.
+#' @param a the hyperparameter (shape1) of the Beta prior for the experimental drug.
+#' @param b the hyperparameter (shape2) of the Beta prior for the experimental drug.
+#' @param p0 the pre-specified reseponse rate.
 #' @param delta the minimally acceptable increment of the response rate for the experimental drug compared with the standard drug.
-#' Note: if type = "superiority", then delta is set to 0.
+#' @param theta the cutoff probability: typically, \eqn{\theta = [0.95, 0.99]} for superiority, \eqn{\theta = [0.01, 0.05]} for futility.
 #' @return
 #' \item{boundset}{the boundaries set; \eqn{U_n} or \eqn{L_n}}
 #' @references
@@ -63,27 +58,27 @@ PostProb <- function(x, n, a, b, p0) {
 #' \emph{Clinical Trial Design: Bayesian and Frequentist Adaptive Methods.}
 #' New York: Wiley.
 #' @examples
-#' stopbound_post(0.05, "futility", 40, 0.6, 1.4, 15, 35, 0)
-#' stopbound_post(0.05, "futility", 30, 0.4, 1.6, 10, 40, 0)
-#' stopbound_post(0.95, "superiority", 40, 0.6, 1.4, 15, 35, 0)
+#' ## Using vague prior Unif(0,1)
+#' PostP.design(type = "futility", nmax=100, a=1, b=1, p0=0.15, delta=0.15, theta=0.05)
+#' PostP.design(type = "efficacy", nmax=100, a=1, b=1, p0=0.15, delta=0.15, theta=0.9)
+#' ## Or using Jeffery prior with Beta(0.5,0.5)
+#' PostP.design(type = "futility", nmax=100, a=0.5, b=0.5, p0=0.15, delta=0.15, theta=0.05)
+#' PostP.design(type = "efficacy", nmax=100, a=0.5, b=0.5, p0=0.15, delta=0.15, theta=0.9)
 #' @export
-PostP.design <- function(type = c("efficacy", "futility"), nmax, a, b, p0, delta, theta) {
-
+PostP.design <- function(type = c("efficacy", "futility"), nmax, a, b, p0, delta=0, theta) {
   type <- match.arg(type)
-
-
   bound <- rep(NA, nmax)
   for (n in 1:nmax) {
     if (type == "efficacy") {
       for (x in 0:n) {
-        if (PostProb(x, n, a, b, p0) > theta) {
+        if (PostP(x, n, a, b, p0) > theta) {
           bound[n] <- x
           break
         }
       }
     } else {
       for (x in n:0) {
-        if (PostProb(x, n, a, b, p0+delta) < theta) {
+        if (PostP(x, n, a, b, p0+delta) < theta) {
           bound[n] <- x
           break
         }
@@ -115,4 +110,3 @@ oc.bdry <- function(pu, pa, r1, n1, r, n){
   names(out) <- c("P(reject H0 | p0)","P(reject H0 | p1)","PET(p0)","EN(p0)")
   out
 }
-
